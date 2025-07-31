e7d469afcec7e939dd2c9b1ba3795291bbfd8d70
Abdouagu <abdessamad.aguezzoul@gmail.com>
2025-07-31 07:36:56 +0100

    Update sidebar-scroll.js

--- src/main/resources/static/AdminBSBMaterialDesign-master/js/sidebar-scroll.js
+++ src/main/resources/static/AdminBSBMaterialDesign-master/js/sidebar-scroll.js
@@ -1,6 +1,6 @@
 /* Active la barre de scroll vertical sur la sidebar gauche */
 $(function () {
-    $('#leftsidebar .menu').slimScroll({
+    $('#leftsidebar .list').slimScroll({
         height: 'calc(100vh - 140px)', // espace disponible sous le profil
         wheelStep: 10,
         touchScrollStep: 50
